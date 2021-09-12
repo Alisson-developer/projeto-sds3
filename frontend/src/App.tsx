2ab03b40b7feed757219d39df8377278574327bb
Alisson-developer <alissonlua7@gmail.com>
2021-09-11 22:19:34 -0300

    Basic Static Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import React from "react";
 
 
 function App() {
   return (
-    <div className="App">
-      <div><h1 className="text-primary">Olá mundo!</h1></div>
-    </div>
+    <React.Fragment>
+      <NavBar />
+      <DataTable />
+      <Footer />
+    </React.Fragment>
   )
 }
 
