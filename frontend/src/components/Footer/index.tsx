1b75a8612e0635e1fe7553b7bc29b55a59072e52
Alisson-developer <alissonlua7@gmail.com>
2021-11-08 23:08:32 -0300

    Grafic bars and Security Config

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/Alisson-developer" target="_blank" rel="noreferrer">Alisson Marques</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/alisson-marques-developer" target="_blank" rel="noreferrer">Alisson Marques</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
                     Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
